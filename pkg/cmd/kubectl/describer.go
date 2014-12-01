52db28e1707d3f56d9597e47613812a630aea940
Michal Fojtik <mfojtik@redhat.com>
2014-12-01 15:13:36 +0100

    Added kubectl Describer for Origin objects

---
+++ pkg/cmd/kubectl/describer.go
@@ -0,0 +1,212 @@
+package kubectl
+
+import (
+	"fmt"
+	"text/tabwriter"
+
+	kctl "github.com/GoogleCloudPlatform/kubernetes/pkg/kubectl"
+	buildapi "github.com/openshift/origin/pkg/build/api"
+	"github.com/openshift/origin/pkg/client"
+)
+
+func DescriberFor(kind string, c *client.Client) (kctl.Describer, bool) {
+	switch kind {
+	case "Build":
+		return &BuildDescriber{
+			BuildClient: func(namespace string) (client.BuildInterface, error) {
+				return c.Builds(namespace), nil
+			},
+		}, true
+	case "Deployment":
+		return &DeploymentDescriber{
+			DeploymentClient: func(namespace string) (client.DeploymentInterface, error) {
+				return c.Deployments(namespace), nil
+			},
+		}, true
+	case "DeploymentConfig":
+		return &DeploymentConfigDescriber{
+			DeploymentConfigClient: func(namespace string) (client.DeploymentConfigInterface, error) {
+				return c.DeploymentConfigs(namespace), nil
+			},
+		}, true
+	case "Image":
+		return &ImageDescriber{
+			ImageClient: func(namespace string) (client.ImageInterface, error) {
+				return c.Images(namespace), nil
+			},
+		}, true
+	case "ImageRepository":
+		return &ImageRepositoryDescriber{
+			ImageRepositoryClient: func(namespace string) (client.ImageRepositoryInterface, error) {
+				return c.ImageRepositories(namespace), nil
+			},
+		}, true
+	case "Route":
+		return &RouteDescriber{
+			RouteClient: func(namespace string) (client.RouteInterface, error) {
+				return c.Routes(namespace), nil
+			},
+		}, true
+	}
+	return nil, false
+}
+
+// BuildDescriber generates information about a build
+type BuildDescriber struct {
+	BuildClient func(namespace string) (client.BuildInterface, error)
+}
+
+func (d *BuildDescriber) DescribeParameters(p buildapi.BuildParameters, out *tabwriter.Writer) {
+	fmt.Fprintf(out, "Strategy:\t%s\n", string(p.Strategy.Type))
+	fmt.Fprintf(out, "Source Type:\t%s\n", string(p.Source.Type))
+	if p.Source.Git != nil {
+		fmt.Fprintf(out, "URL:\t%s\n", string(p.Source.Git.URI))
+		if len(p.Source.Git.Ref) > 0 {
+			fmt.Fprintf(out, "Ref:\t%s\n", string(p.Source.Git.Ref))
+		}
+	}
+	fmt.Fprintf(out, "Output Image:\t%s\n", string(p.Output.ImageTag))
+	fmt.Fprintf(out, "Output Registry:\t%s\n", string(p.Output.Registry))
+}
+
+func (d *BuildDescriber) Describe(namespace, name string) (string, error) {
+	bc, err := d.BuildClient(namespace)
+	if err != nil {
+		return "", err
+	}
+	build, err := bc.Get(name)
+	if err != nil {
+		return "", err
+	}
+
+	return tabbedString(func(out *tabwriter.Writer) error {
+		formatMeta(out, build.ObjectMeta)
+		fmt.Fprintf(out, "Status:\t%s\n", string(build.Status))
+		fmt.Fprintf(out, "Build Pod:\t%s\n", string(build.PodName))
+		d.DescribeParameters(build.Parameters, out)
+		return nil
+	})
+}
+
+// DeploymentDescriber generates information about a deployment
+type DeploymentDescriber struct {
+	DeploymentClient func(namespace string) (client.DeploymentInterface, error)
+}
+
+func (d *DeploymentDescriber) Describe(namespace, name string) (string, error) {
+	bc, err := d.DeploymentClient(namespace)
+	if err != nil {
+		return "", err
+	}
+	deployment, err := bc.Get(name)
+	if err != nil {
+		return "", err
+	}
+
+	return tabbedString(func(out *tabwriter.Writer) error {
+		formatMeta(out, deployment.ObjectMeta)
+		fmt.Fprintf(out, "Status:\t%s\n", string(deployment.Status))
+		fmt.Fprintf(out, "Strategy:\t%s\n", string(deployment.Strategy.Type))
+		fmt.Fprintf(out, "Causes:\n")
+		for _, c := range deployment.Details.Causes {
+			fmt.Fprintf(out, "\t\t%s\n", string(c.Type))
+		}
+		// TODO: Add description for controllerTemplate
+		return nil
+	})
+}
+
+// DeploymentConfigDescriber generates information about a DeploymentConfig
+type DeploymentConfigDescriber struct {
+	DeploymentConfigClient func(namespace string) (client.DeploymentConfigInterface, error)
+}
+
+func (d *DeploymentConfigDescriber) Describe(namespace, name string) (string, error) {
+	bc, err := d.DeploymentConfigClient(namespace)
+	if err != nil {
+		return "", err
+	}
+	deploymentConfig, err := bc.Get(name)
+	if err != nil {
+		return "", err
+	}
+
+	return tabbedString(func(out *tabwriter.Writer) error {
+		formatMeta(out, deploymentConfig.ObjectMeta)
+		fmt.Fprintf(out, "Latest Version:\t%s\n", string(deploymentConfig.LatestVersion))
+		fmt.Fprintf(out, "Triggers:\t\n")
+		for _, t := range deploymentConfig.Triggers {
+			fmt.Fprintf(out, "Type:\t%s\n", t.Type)
+		}
+		return nil
+	})
+}
+
+// ImageDescriber generates information about a Image
+type ImageDescriber struct {
+	ImageClient func(namespace string) (client.ImageInterface, error)
+}
+
+func (d *ImageDescriber) Describe(namespace, name string) (string, error) {
+	bc, err := d.ImageClient(namespace)
+	if err != nil {
+		return "", err
+	}
+	image, err := bc.Get(name)
+	if err != nil {
+		return "", err
+	}
+
+	return tabbedString(func(out *tabwriter.Writer) error {
+		formatMeta(out, image.ObjectMeta)
+		fmt.Fprintf(out, "Docker Image:\t%s\n", string(image.DockerImageReference))
+		return nil
+	})
+}
+
+// ImageRepositoryDescriber generates information about a ImageRepository
+type ImageRepositoryDescriber struct {
+	ImageRepositoryClient func(namespace string) (client.ImageRepositoryInterface, error)
+}
+
+func (d *ImageRepositoryDescriber) Describe(namespace, name string) (string, error) {
+	bc, err := d.ImageRepositoryClient(namespace)
+	if err != nil {
+		return "", err
+	}
+	imageRepository, err := bc.Get(name)
+	if err != nil {
+		return "", err
+	}
+
+	return tabbedString(func(out *tabwriter.Writer) error {
+		formatMeta(out, imageRepository.ObjectMeta)
+		fmt.Fprintf(out, "Tags:\t%s\n", formatLabels(imageRepository.Tags))
+		fmt.Fprintf(out, "Docker Repository:\t%s\n", string(imageRepository.DockerImageRepository))
+		return nil
+	})
+}
+
+// RouteDescriber generates information about a Route
+type RouteDescriber struct {
+	RouteClient func(namespace string) (client.RouteInterface, error)
+}
+
+func (d *RouteDescriber) Describe(namespace, name string) (string, error) {
+	bc, err := d.RouteClient(namespace)
+	if err != nil {
+		return "", err
+	}
+	route, err := bc.Get(name)
+	if err != nil {
+		return "", err
+	}
+
+	return tabbedString(func(out *tabwriter.Writer) error {
+		formatMeta(out, route.ObjectMeta)
+		fmt.Fprintf(out, "Host:\t%s\n", string(route.Host))
+		fmt.Fprintf(out, "Path:\t%s\n", string(route.Path))
+		fmt.Fprintf(out, "Service Name:\t%s\n", string(route.ServiceName))
+		return nil
+	})
+}
---
+++ pkg/cmd/kubectl/helpers.go
@@ -0,0 +1,41 @@
+package kubectl
+
+import (
+	"bytes"
+	"fmt"
+	"text/tabwriter"
+
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/labels"
+)
+
+func tabbedString(f func(*tabwriter.Writer) error) (string, error) {
+	out := new(tabwriter.Writer)
+	b := make([]byte, 1024)
+	buf := bytes.NewBuffer(b)
+	out.Init(buf, 0, 8, 1, '\t', 0)
+
+	err := f(out)
+	if err != nil {
+		return "", err
+	}
+
+	out.Flush()
+	str := string(buf.String())
+	return str, nil
+}
+
+func formatLabels(labelMap map[string]string) string {
+	l := labels.Set(labelMap).String()
+	if l == "" {
+		l = "<none>"
+	}
+	return l
+}
+
+func formatMeta(out *tabwriter.Writer, m api.ObjectMeta) {
+	fmt.Fprintf(out, "Name:\t%s\n", m.Name)
+	fmt.Fprintf(out, "Annotations:\t%s\n", formatLabels(m.Annotations))
+	fmt.Fprintf(out, "Labels:\t%s\n", formatLabels(m.Labels))
+	fmt.Fprintf(out, "Created:\t%s\n", m.CreationTimestamp)
+}
