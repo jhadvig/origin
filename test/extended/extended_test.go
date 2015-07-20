808dd93d93f619598773512390defb90cd87e478
Michal Fojtik <mfojtik@redhat.com>
2015-07-20 17:42:49 +0200

    Minor refactoring

--- test/extended/extended_test.go
+++ test/extended/extended_test.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"path/filepath"
 	"testing"
 
 	"github.com/GoogleCloudPlatform/kubernetes/pkg/client/clientcmd"
@@ -15,29 +14,19 @@ import (
 	"github.com/onsi/ginkgo/config"
 	"github.com/onsi/ginkgo/reporters"
 	"github.com/onsi/gomega"
-	exutil "github.com/openshift/origin/test/extended/util"
 )
 
 var (
 	reportDir = flag.String("report-dir", "", "Path to the directory where the JUnit XML reports should be saved. Default is empty, which doesn't generate these reports.")
 )
 
+// init initialize the extended testing suite.
+// You can set these environment variables to configure extended tests:
+// MASTER_ADDR - The host or apiserver to connect to
+// SERVER_CONFIG_DIR - Directory where OpenShift stores configuration
+// SERVER_KUBECONFIG_PATH - Location of 'admin.kubeconfig' file
+// SERVER_CERT_DIR - Directory with OpenShift client certificates
 func init() {
-	exutil.RequireServerVars()
-
-	var testKubeConfig, testKubeCert string
-
-	kubeConfigPath := os.Getenv("SERVER_KUBECONFIG_PATH")
-	if len(kubeConfigPath) == 0 {
-		testKubeConfig = filepath.Join(os.Getenv("SERVER_CONFIG_DIR"), "master", "admin.kubeconfig")
-		testKubeCert = filepath.Join(os.Getenv("SERVER_CONFIG_DIR"), "master")
-	} else {
-		testKubeConfig = filepath.Join(os.Getenv("SERVER_CONFIG_DIR"), kubeConfigPath)
-		testKubeCert = filepath.Join(os.Getenv("SERVER_CERT_DIR"))
-	}
-
-	testContext := TestContextType{}
-
 	// Turn on verbose by default to get spec names
 	config.DefaultReporterConfig.Verbose = true
 
@@ -47,13 +36,13 @@ func init() {
 	// Randomize specs as well as suites
 	config.GinkgoConfig.RandomizeAllSpecs = false
 
-	flag.StringVar(&testContext.KubeConfig, clientcmd.RecommendedConfigPathFlag, testKubeConfig, "Path to kubeconfig containing embeded authinfo.")
+	flag.StringVar(&testContext.KubeConfig, clientcmd.RecommendedConfigPathFlag, adminKubeConfigPath(), "Path to kubeconfig containing embeded authinfo.")
+	flag.StringVar(&testContext.CertDir, "cert-dir", kubeCertPath(), "Path to the directory containing the certs. Default is empty, which doesn't use certs.")
 	flag.StringVar(&testContext.KubeContext, clientcmd.FlagContext, "", "kubeconfig context to use/override. If unset, will use value from 'current-context'")
-	flag.StringVar(&testContext.CertDir, "cert-dir", testKubeCert, "Path to the directory containing the certs. Default is empty, which doesn't use certs.")
 	flag.StringVar(&testContext.Host, "host", os.Getenv("MASTER_ADDR"), "The host, or apiserver, to connect to")
-	flag.StringVar(&testContext.KubectlPath, "kubectl-path", "kubectl", "The kubectl binary to use. For development, you might use 'cluster/kubectl.sh' here.")
-	flag.StringVar(&testContext.OutputDir, "e2e-output-dir", "/tmp", "Output directory for interesting/useful test data, like performance data, benchmarks, and other metrics.")
+	flag.StringVar(&testContext.OutputDir, "e2e-output-dir", os.TempDir(), "Output directory for interesting/useful test data, like performance data, benchmarks, and other metrics.")
 
+	// Override the default Kubernetes E2E configuration
 	SetTestContext(testContext)
 }
 
@@ -76,6 +65,6 @@ func TestExtended(t *testing.T) {
 	if *reportDir != "" {
 		r = append(r, reporters.NewJUnitReporter(path.Join(*reportDir, fmt.Sprintf("junit_%02d.xml", config.GinkgoConfig.ParallelNode))))
 	}
-  
+
 	ginkgo.RunSpecsWithDefaultAndCustomReporters(t, "OpenShift extended tests suite", r)
 }
